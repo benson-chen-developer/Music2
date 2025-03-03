11c133d5eda698daed242cf329d39e8b23de6a05
Benson Chen <bensonchen456@gmail.com>
2025-03-03 00:52:53 -0500

    Add project files.

---
+++ Music2/Context/MyMusicDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Music2.Models.Auth;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using MyMusic.Core.Models;
+using MyMusic.Core.Models.Auth;
+using MyMusic.Data.Configurations;
+
+namespace Music2.Context
+{
+    public class MyMusicDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        public MyMusicDbContext(DbContextOptions<MyMusicDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder
+                .ApplyConfiguration(new MusicConfiguration());
+
+            builder
+                .ApplyConfiguration(new ArtistConfiguration());
+        }
+    }
+}
---
+++ Music2/Models/Auth/Role.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Music2.Models.Auth
+{
+    public class Role
+    {
+
+    }
+}
